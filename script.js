df9b83e72034a60af2a60c25e92b7ef5140dc9b0
mutaqimuni <mutaqim.unijaya@gmail.com>
2022-08-28 06:53:29 +0800

    declare total pokemon sedia ada 150. declare variable supaya senang guna dalam for loop nanti

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 //dapatkan wrapper dalam html tadi dan declare variable
 const poke_cointainer = getElementById('poke_container');
+
+//declare total pokemon sedia ada 150. declare variable supaya senang guna dalam for loop nanti
+const pokemons_number = 150;
