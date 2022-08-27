83c0de8a894bf9a7d30297c149c4ee879e9f5138
mutaqimuni <mutaqim.unijaya@gmail.com>
2022-08-28 06:56:23 +0800

    function yang akan dicall untuk dapatkan pokemon.

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const poke_cointainer = getElementById('poke_container');
 
 //declare total pokemon sedia ada 150. declare variable supaya senang guna dalam for loop nanti
 const pokemons_number = 150;
+
+//function yang akan dicall untuk dapatkan pokemon.
+//jadikan async function supaya banyak lagi function boleh followup lepas function ni initiate the call
+//getPokemon akan perlukan id . kalau lebih  1 parameter kena letak dalam () cth , (id, name)
+const getPokemon = async id => {
+    
+}
